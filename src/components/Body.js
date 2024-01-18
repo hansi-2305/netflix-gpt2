4b776d0492af0663206823f60a26e4bf27c6942a
Hansi <hanshikaag2021@gmail.com>
2024-01-18 21:58:32 +0530

    signin/sign up form && Login /header

---
+++ src/components/Body.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Login from './Login'
+import Browse from './Browse'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+
+const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path:"/",
+            element:<Login/>
+        },
+        {
+            path:"/browse",
+            element:<Browse/>
+
+        },
+    ])
+
+
+  return (
+    <div>
+
+
+        {/* <Login/>
+        <Browse/> */}
+        {/* isko esse likhne k jagh j=niche wala tareeka use karte hai */}
+        
+        <RouterProvider router={appRouter}/>
+
+
+
+
+    </div>
+  )
+}
+
+export default Body
